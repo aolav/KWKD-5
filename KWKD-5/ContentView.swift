f4b093517ee82934e1c0552a73d7ba451c73dfcb
Andrea O <17aolav@gmail.com>
2023-06-26 10:13:55 -0400

    Initial Commit

---
+++ KWKD-5/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  KWKD-5
+//
+//  Created by Andrea Olavarrieta on 6/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KWKD-5/KWKD_5App.swift
@@ -0,0 +1,17 @@
+//
+//  KWKD_5App.swift
+//  KWKD-5
+//
+//  Created by Andrea Olavarrieta on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct KWKD_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
